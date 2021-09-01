a2a159920439c4b0fabbcbd7d320bb76d119388b
GUILHERME LEDIER ALVES <21192997@CFP127ALUNO.LOCAL>
2021-09-01 10:16:54 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
 "use strict"
 
-const lampada = document.getElementById("lampada")
+// function getId(elemento) {
+//     return document.getElementById(elemento)
+// }
 
-function id(elemento) {
-    return document.getElementById(elemento)
+const getId = (elemento) => document.getElementById(elemento)
+
+const lampada = getId("lampada")
+let idLigar
+let idDesligar
+
+const botoesLigaDesliga = (estadoLigado, estadoDesligado, estadoPiscar) => {
+    const ligar = getId("ligar")
+    const desligar = getId("desligar")
+    const piscar = getId("piscar")
+
+    ligar.disabled = estadoLigado
+    desligar.disabled = estadoDesligado
+    piscar.disabled = estadoPiscar
 }
 
-function botoesLigaDesliga(estadoLiga, estadoDesliga) {
-    const botaoLigar = document.getElementById("ligar")
-    const botaoDesligar = document.getElementById("desligar")
-    botaoLigar.disabled = estadoLiga
-    botaoDesligar.disabled = estadoDesliga
+const lampadaQuebrada = () => {
+    return lampada.src.indexOf("quebrada") !== -1
 }
 
-function lampadaQuebrada() {
-    return lampada.src.includes("quebrada")
-}
-
-function ligarLampada() {
+const ligarLampada = () => {
     if (!lampadaQuebrada()) {
-        lampada.src = "img/ligada.jpg"
-        botoesLigaDesliga(true, false)
+        lampada.src = 'img/ligada.jpg'
+        botoesLigaDesliga(true, false, false)
     }
 }
 
-function desligarLampada() {
+const desligarLampada = () => {
     if (!lampadaQuebrada()) {
-        lampada.src = "img/desligada.jpg"
-        botoesLigaDesliga(false, true)
+        lampada.src = 'img/desligada.jpg'
+        botoesLigaDesliga(false, true, false)
     }
 }
 
-function quebrarLampada() {
+const quebrarLampada = () => {
     lampada.src = "img/quebrada.jpg"
-    botoesLigaDesliga(true, true)
+    botoesLigaDesliga(true, true, true)
 }
 
-// eventos
-id("ligar").addEventListener("click", ligarLampada)
+const pararPiscar = () => {
+    clearInterval(idLigar)
+    clearInterval(idDesligar)
+}
 
-id('desligar').addEventListener("click", desligarLampada)
+const piscarLampada = () => {
+    const botaoPiscar = getId("piscar")
 
-id("lampada").addEventListener("mouseover", ligarLampada)
+    if (botaoPiscar.textContent == "Piscar") {
+        idLigar = setInterval(ligarLampada, 500)
+        idDesligar = setInterval(desligarLampada, 1000)
+        botaoPiscar.textContent = "Parar"
+    } else {
+        pararPiscar()
+        botaoPiscar.textContent = "Piscar"
+    }
+}
 
-id("lampada").addEventListener("mouseleave", desligarLampada)
+//Eventos
+getId("ligar")
+    .addEventListener("click", ligarLampada)
 
-id("lampada").addEventListener("dblclick", quebrarLampada)
+getId("desligar")
+    .addEventListener("click", desligarLampada)
+
+getId("lampada")
+    .addEventListener("mouseover", ligarLampada)
+
+getId("lampada")
+    .addEventListener("mouseleave", desligarLampada)
+
+getId("lampada")
+    .addEventListener("dblclick", quebrarLampada)
+
+getId("piscar")
+    .addEventListener("click", piscarLampada)
